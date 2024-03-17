76f524b7da457af16cc3929f5648eabaf3fa9038
Ujjawal Patidar <ujjawalp900@gmail.com>
2024-03-17 17:22:49 +0530

    new files create

---
+++ src/store/authSlice.js
@@ -0,0 +1,21 @@
+import { combineReducers, createSlice } from "@reduxjs/toolkit";
+
+const initialState = { status: false, userData: null }
+
+const authSlice = createSlice(combineReducers({
+    name: "auth",
+    initialState,
+    reducers: {
+        login: (state, action) => {
+            state.status = true;
+            state.userData = action.payload.userData;
+        },
+        logout: (state) => {
+            state.status = false;
+            state.userData = null;
+        }
+    }
+}))
+
+export const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
